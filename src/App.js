417aaf5b4520368da1c320b4c192424feaabb356
luis <luisgonzalez18997@gmail.com>
2019-01-07 13:02:29 -0400

    add connection with socket-server

--- src/App.js
+++ src/App.js
@@ -1,28 +1,53 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import socketIOClient from 'socket.io-client'
 
+// Making the App component
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+  constructor() {
+    super()
 
-export default App;
+    this.state = {
+      endpoint: "http://localhost:5050", // this is where we are connecting to with sockets
+      color: 'white'
+        ///
+
+      };
+    }
+
+    // sending sockets
+    send = () => {
+      const socket = socketIOClient(this.state.endpoint);
+      socket.emit('change color', this.state.color) // change 'red' to this.state.color
+    }
+
+    ///
+
+    // adding the function
+    setColor = (color) => {
+      this.setState({ color })
+    }
+
+    ///
+
+    render() {
+      // testing for socket connections
+
+      const socket = socketIOClient(this.state.endpoint);
+      socket.on('change color', (col) => {
+        document.body.style.backgroundColor = col
+      })
+
+      return (
+        <div style={{ textAlign: "center" }}>
+          <button onClick={() => this.send() }>Change Color</button>
+
+          <button id="blue" onClick={() => this.setColor('blue')}>Blue</button>
+          <button id="red" onClick={() => this.setColor('red')}>Red</button>
+          <button id="yellow" onClick={() => this.setColor('yellow')}>yellow</button>
+        <button id="green" onClick={() => this.setColor('green')}>green</button>
+
+        </div>
+      )
+    }
+  }
+  export default App;
